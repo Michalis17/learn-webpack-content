c60bf70d729832d4e0f8e026792c9e9cb8772f2e
Michalis <mike.michaelides17@gmail.com>
2023-07-13 13:31:05 +0300

    little change

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,9 @@ const path = require("path")
 const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
+    entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    entry: './src/client/index.js',
     module: {
         rules: [
             {
